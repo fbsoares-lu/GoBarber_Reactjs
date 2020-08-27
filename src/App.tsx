555de8d051aa723400e1c1d1251355eabc5d444b
Lucas Soares <lsoareshfb@gmail.com>
2020-08-27 11:06:04 -0300

    Criação do context

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn/index';
-import SignUp from './pages/SignUp/index';
+// import SignUp from './pages/SignUp/index';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
     <>
-        <SignIn />
+        <AuthContext.Provider value={{ name: 'Lucas' }}>
+            <SignIn />
+        </AuthContext.Provider>
         <GlobalStyle />
     </>
 );
